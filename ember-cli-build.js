b8140e6094f5223a90fcacbfe1e07bf9c8d23558
Ivan Votti <ivanvotti@gmail.com>
2016-04-18 17:32:45 +0700

    Setup SVG icons system

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,17 @@
-/*jshint node:true*/
-/* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    svgstore: {
+      files: {
+        sourceDirs: 'icons',
+        outputFile: '/assets/icons.svg'
+      }
+    },
+
+    fingerprint: {
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'svg']
+    }
   });
 
   app.import('bower_components/normalize-css/normalize.css');
