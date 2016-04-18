5d58f1107dd3a10aee67b21ee48ad0ad7507dd80
Ivan Votti <ivanvotti@gmail.com>
2016-04-18 17:22:11 +0700

    Add ember-cli-template-lint

---
+++ .template-lintrc.js
@@ -0,0 +1,10 @@
+/* jshint node:true */
+'use strict';
+
+module.exports = {
+  // 'bare-strings': ['(', ')', ',', '.', '&', '+', '-', '=', '*', '/', '#', '%', '!', '?', ':', '[', ']', '{', '}'],
+  'bare-strings': false,
+  'block-indentation': 2,
+  'html-comments': true,
+  'triple-curlies': true
+};
