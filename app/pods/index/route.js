dae7d7c7652b424dfcef126f35c68583d4b0288b
Ivan Votti <ivanvotti@gmail.com>
2016-04-22 00:19:43 +0700

    Move from index to timer route

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,30 +1,7 @@
 import Ember from 'ember';
-import moment from 'moment';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('time-entry');
-  },
-
-  actions: {
-    addTimeEntry(entryName) {
-      return this.store.createRecord('time-entry', {
-        name: entryName,
-        startedAt: new Date()
-      })
-      .save();
-    },
-
-    stopTimeEntry(timeEntry) {
-      let startedAt = timeEntry.get('startedAt');
-      let stoppedAt = new Date();
-      let duration = moment(stoppedAt).diff(startedAt, 'seconds');
-
-      timeEntry.setProperties({
-        stoppedAt,
-        duration
-      });
-      return timeEntry.save();
-    }
+  beforeModel() {
+    this.transitionTo('timer');
   }
 });
