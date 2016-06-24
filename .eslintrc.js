15e4fc135677c687736a94d69507d6f5ee900dae
Ivan Votti <ivanvotti@gmail.com>
2016-06-24 19:08:51 +0700

    Update eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  parser: 'babel-eslint',
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true
+  },
+
+  rules: {
+  }
+};
