25c5341155b65a9475c1b24dff3bf659d5038359
Ivan Votti <ivanvotti@gmail.com>
2016-06-24 19:49:33 +0700

    Add airbnb-base and some custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,30 @@
 module.exports = {
-  extends: 'eslint:recommended',
-  parser: 'babel-eslint',
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',  // for async/await and decorators
 
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module'
   },
 
-  env: {
-    browser: true
-  },
-
   rules: {
+    'prefer-const': 0,
+    'comma-dangle': 0,
+    'prefer-arrow-callback': 0,
+    'func-names': 0,
+    'prefer-rest-params': 0,
+    'no-underscore-dangle': 0,
+    'array-callback-return': 0,
+    'import/no-unresolved': 0,
+
+    'space-before-function-paren': [2, {
+      'anonymous': 'never',
+      'named': 'never'
+    }],
+
+    'generator-star-spacing': [2, {
+      'before': false,
+      'after': true
+    }]
   }
 };
