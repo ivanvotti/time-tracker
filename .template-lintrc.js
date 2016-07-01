07faaba36d2b5d95525c7e07fe783ee1d2b07aae
Ivan Votti <ivanvotti@gmail.com>
2016-07-01 16:08:01 +0700

    Update template lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,9 +1,5 @@
 'use strict';
 
 module.exports = {
-  // 'bare-strings': ['(', ')', ',', '.', '&', '+', '-', '=', '*', '/', '#', '%', '!', '?', ':', '[', ']', '{', '}'],
-  'bare-strings': false,
-  'block-indentation': 2,
-  'html-comments': true,
-  'triple-curlies': true
+  extends: 'recommended'
 };
